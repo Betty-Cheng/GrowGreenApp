fa56b35c5158aa7bf9577a34aea92f9976e218be
Vaishnavi Annamraju <n_pranathi1@yahoo.com>
2021-06-30 22:39:23 -0700

    Added a view control for the plants list. also edited the about us message for me.

--- GrowGreenApp/ResourcesViewController.swift
+++ GrowGreenApp/ResourcesViewController.swift
@@ -9,11 +9,42 @@ import UIKit
 
 class ResourcesViewController: UIViewController {
 
+    @IBOutlet weak var openDIYRecipesButton: UIButton!
+    
+    @IBOutlet weak var openGardenSuppliesButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        openDIYRecipesButton.addTarget(self, action: #selector(openDIYRecipes), for: .touchUpInside)
+        openGardenSuppliesButton.addTarget(self, action: #selector(openGardenSupplies), for: .touchUpInside)
     }
+    
+    @objc func openDIYRecipes() {
+        if let urlToOpen = URL(string: "https://homebnc.com/best-creative-garden-container-ideas/") {
+            UIApplication.shared.open(urlToOpen, options: [:]) { (done) in
+                print("Link was opened successfully")
+            }
+    
+        }
+        
+            
+    }
+    
+    @objc func openGardenSupplies() {
+        if let urlToOpen = URL(string: "https://www.gardendesign.com/how-to/tools.html") {
+            UIApplication.shared.open(urlToOpen, options: [:]) { (done) in
+                print("Link was opened successfully")
+            }
+    
+        }
+        
+            
+    }
+    
+
     
 
     /*
