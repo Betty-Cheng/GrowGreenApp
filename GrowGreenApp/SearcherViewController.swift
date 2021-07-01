c63cdd1a2a48e965c5ea2bdef47846f089bc9fbe
Vaishnavi Annamraju <n_pranathi1@yahoo.com>
2021-06-30 17:50:10 -0700

    Changed the link of the google maps to automatically have "nearby community gardens" typed in.

--- GrowGreenApp/SearcherViewController.swift
+++ GrowGreenApp/SearcherViewController.swift
@@ -19,7 +19,7 @@ class SearcherViewController: UIViewController {
     }
     
     @objc func openGoogleMaps() {
-        if let urlToOpen = URL(string: "https://googlemaps.com") {
+        if let urlToOpen = URL(string: "https://www.google.com/search?tbs=lf:1,lf_ui:1&tbm=lcl&sxsrf=ALeKk03eEatgx5Z_bDUwCIhWR5RBiVnDhg:1625100367980&q=nearby+community+garden&rflfq=1&num=10&sa=X&ved=2ahUKEwjjiqHR0sDxAhXJ-Z4KHd9jABIQjGp6BAgmEEo&biw=1440&bih=789&dpr=2#rlfi=hd:;si:;mv:[[47.7632349,-121.9807255],[47.4698746,-122.3527488]];tbs:lrf:!1m4!1u3!2m2!3m1!1e1!1m4!1u2!2m2!2m1!1e1!2m1!1e2!2m1!1e3!3sIAE,lf:1,lf_ui:1") {
             UIApplication.shared.open(urlToOpen, options: [:]) { (done) in
                 print("Link was opened successfully")
             }
