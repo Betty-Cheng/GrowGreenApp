35b1a5d414803f26aac1ce8c71ae578ff2bbe075
Betty Cheng <bettycheng000@gmail.com>
2021-07-01 11:59:15 -0700

    finished the about us page, added in the buttons for the recipes page, finished adding the segues for each plant page

---
+++ GrowGreenApp/LettuceViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LettuceViewController.swift
+//  GrowGreenApp
+//
+//  Created by  Scholar on 7/1/21.
+//
+
+import UIKit
+
+class LettuceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
