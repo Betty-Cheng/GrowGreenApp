169d34ba9d31e4e312fdea448a55d3f1624c7e97
Vaishnavi Annamraju <n_pranathi1@yahoo.com>
2021-07-01 18:31:10 -0700

    Added transplant calculator codes and layout for spring onion and cucumber. Did the layout for the pepper page. I haven't done the peppers page yet.

--- GrowGreenApp/CucumbersViewController.swift
+++ GrowGreenApp/CucumbersViewController.swift
@@ -8,22 +8,41 @@
 import UIKit
 
 class CucumbersViewController: UIViewController {
+    
+    @IBOutlet weak var cucumberSlider: UISlider!
+    @IBOutlet weak var cucumberInformation: UILabel!
+    
+    var cucumberPlantTime = 35
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        cucumberSlider.setValue(35, animated: true)
+        cucumberInformation.text = "It's been \(cucumberPlantTime) days since you've sown the seed!"
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func cucumberNumberOfDaysSincePlanting(_ sender: UISlider) {
+        cucumberPlantTime = Int(sender.value)
+        cucumberInformation.text = "It's been \(cucumberPlantTime) days since you've sown the seed!"
     }
-    */
+    
+    
+    @IBAction func calculateCucumberDays(_ sender: UIButton) {
+        if 21 ... 28 ~= cucumberPlantTime {
+          cucumberInformation.text = "It's the perfect time to transplant!"
+        } else if cucumberPlantTime >= 29 {
+          cucumberInformation.text = "Transplanting is wayy overdue!"
+        } else if cucumberPlantTime < 20 {
+          cucumberInformation.text = "Check back in \(21 - cucumberPlantTime) days!"
+        } else if cucumberPlantTime == 20 {
+          cucumberInformation.text = "Check back in a day!"
+        }
+        
+        cucumberSlider.setValue (50, animated: true)
+    }
+    
+
 
 }
--- GrowGreenApp/SpringOnionsViewController.swift
+++ GrowGreenApp/SpringOnionsViewController.swift
@@ -8,22 +8,67 @@
 import UIKit
 
 class SpringOnionsViewController: UIViewController {
+    
+    @IBOutlet weak var springOnionSlider: UISlider!
+    
+    @IBOutlet weak var springOnionInformation: UILabel!
+    
+    var springOnionPlantTime = 35
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        springOnionSlider.setValue(35, animated: true)
+        springOnionInformation.text = "It's been \(springOnionPlantTime) days since you've sown the seed!"
 
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func springOnionNumberOfDaysSincePlanting(_ sender: UISlider) {
+        springOnionPlantTime = Int(sender.value)
+        springOnionInformation.text = "It's been \(springOnionPlantTime) days since you've sown the seed!"
+    }
+    
+    @IBAction func calculateSpringOnionDays(_ sender: UIButton) {
+        if 7 ... 14 ~= springOnionPlantTime{
+            springOnionInformation.text = "It's the perfect time to transplant!"
+          } else if springOnionPlantTime >= 15 {
+            springOnionInformation.text = "Transplanting is wayy overdue!"
+          } else if springOnionPlantTime < 6 {
+           springOnionInformation.text = "Check back in \(7 - springOnionPlantTime) days!"
+          } else if springOnionPlantTime == 6{
+            springOnionInformation.text = "Check back in a day!"
+          }
+        springOnionSlider.setValue (50, animated: true)
+        
+    }
+    
 
     /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
+     
+     @IBOutlet weak var lettuceInformation: UILabel!
+     
+     @IBAction func lettuceNumberOfDaysSincePlanting(_ sender: UISlider) {
+         lettucePlantTime = Int(sender.value)
+         lettuceInformation.text = "It's been \(lettucePlantTime) days since you've sown the seed!"
+     }
+     
+     
+     @IBAction func calculateLettuceDays(_ sender: UIButton) {
+         if 21 ... 28 ~= lettucePlantTime {
+           lettuceInformation.text = "It's the perfect time to transplant!"
+         } else if lettucePlantTime >= 29 {
+           lettuceInformation.text = "Transplanting is wayy overdue!"
+         } else if lettucePlantTime < 20 {
+           lettuceInformation.text = "Check back in \(21 - lettucePlantTime) days!"
+         } else if lettucePlantTime == 20 {
+          lettuceInformation.text = "Check back in a day!"
+         }
+         
+         lettuceSlider.setValue(50, animated: true)
+     }
+     
     */
 
 }
